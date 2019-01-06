a2cbec4451b0859fa70836d8282b2bff447ed0ad
victory-clan <46355716+victory-clan@users.noreply.github.com>
2019-01-06 22:54:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -307,4 +307,20 @@ channel.guild.owner.send(`<@!${channelremover.id}>
   },Otime)
   });
   
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+// -say
+  if (command === "say") {
+          message.delete()
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+      }
+});
+
   client.login(process.env.BOT_TOKEN);
