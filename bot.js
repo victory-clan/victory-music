10b5f1d195a4d7b3ee7c9c17016e4b4be198a71f
victory-clan <46355716+victory-clan@users.noreply.github.com>
2019-01-29 14:25:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,7 +88,7 @@ client.on('message', async msg => {
                     .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
  
-                    .setFooter("By iiFireKingYTii_#7310")
+                    .setFooter("By !ν¢ |Bako gaming#6414")
                     msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
                    
                     // eslint-disable-next-line max-depth
